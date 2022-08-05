329db16337d1d6a948d6e3b76f57d335f46932fa
zhangze <1183460943@qq.com>
2022-08-05 19:32:23 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,6 @@ import HelloWorld from './components/HelloWorld.vue'
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Vite + zhangze" />
 </template>
 
 <style scoped>
